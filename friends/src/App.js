d086928b59bc04a8ddf6b397d1072fc3e5a2ab92
Tyler Biswell <tsbiswell@gmail.com>
2019-09-18 10:18:18 -0600

    add login page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+
+import PrivateRoute from './components/PrivateRoute';
+import Login from './components/Login';
+import FriendsList from './components/FriendsList';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'></header>
-    </div>
+    <Router>
+      <div className='App'>
+        <ul>
+          <li>
+            <Link to='/login'>Login</Link>
+            <Link to='/friendslist'>Friends</Link>
+          </li>
+        </ul>
+      </div>
+      <Switch>
+        <Route path='/login' component={Login} />
+        <PrivateRoute path='/friendslist' component={FriendsList} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ friends/src/components/Login.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { Redirect } from 'react-router-dom';
+
+export default function Login(props) {
+  const [credentials, setCredentials] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = e =>
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+
+  const login = e => {
+    e.preventDefault();
+
+    axiosWithAuth()
+      .post('/login', credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/friendslist');
+      })
+      .catch(err => console.log(err));
+  };
+
+  if (localStorage.getItem('token')) return <Redirect to='/friendslist' />;
+
+  return (
+    <div>
+      <form onSubmit={login}>
+        <input
+          type='text'
+          name='username'
+          placeholder='...username'
+          autoComplete='off'
+          value={credentials.username}
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          name='password'
+          placeholder='...password'
+          autoComplete='off'
+          value={credentials.password}
+          onChange={handleChange}
+        />
+        <button>Log in</button>
+      </form>
+    </div>
+  );
+}
